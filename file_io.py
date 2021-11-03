883491c8977b9216ea366f931b0c0803dbe37f77
Ivan Zurilov <johnydevprofile@gmail.com>
2021-11-03 20:38:00 +0200

    some file operations

---
+++ file_io.py
@@ -0,0 +1,8 @@
+# file = open('article.txt', 'r')
+# print(file.read()) #read the entire file
+# print(file.read(64)) #read the first 64 characters
+# print(file.readline()) #read the first line 
+# file.close()
+
+with open('article.txt', 'r') as same_file: #open file using exception handling
+    print(same_file.readline())
