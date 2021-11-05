a7dea248e22ca7b669591c226b50be35ffc239cf
Ivan Zurilov <johnydevprofile@gmail.com>
2021-11-05 23:51:39 +0200

    oop basics

---
+++ oop/employee.py
@@ -0,0 +1,41 @@
+#class that inherits from Human
+from oop.human import Human
+
+class Employee(Human):
+    position = 'trainee'
+    chef = 'anyone'
+    experience = 0
+    departament = 'basement dweller'
+    duty = 'drop the production db'
+    salary = 100
+
+    def __init__(self):
+        super().__init__()
+    
+    #overriden methods
+    def eat(self, food):
+        return f"Employees eat {super.eat(food)}"
+
+    #overloaded methods
+    def set_bank_acc_password(self, password, crap = 'asdf1337'):
+        if self.__bank_acc_password is not crap:
+            self.__bank_acc_password = password
+        else:
+            self.__bank_acc_password = 'flgvjafghapofghfg'
+        
+    #methods
+    def get_boss_mad(self):
+        self.experience = 0
+        self.salary = 0
+        self.position = 'trainee'
+        self.chef = 'anyone'
+        self.departament = 'basement dweller'
+        self.duty = 'drop the production db'        
+        print('All your stats dropped to the very bottom')
+
+    def get_promotion(self, position, departament, duty, salary):
+        self.position = position
+        self.departament = departament
+        self.duty = duty
+        self.salary = salary
+        print('You got new benefits and obligations')
